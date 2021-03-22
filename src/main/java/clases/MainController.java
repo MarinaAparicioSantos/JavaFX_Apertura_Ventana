278642e8790220c8f27376d3b781fd09d0acb59f
MarinaAparicioSantos <marina.aparicio.santos.al@iespoligonosur.org>
2021-03-22 19:16:38 +0100

    22/03

---
+++ src/main/java/clases/MainController.java
@@ -0,0 +1,20 @@
+package clases;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+    @FXML
+    private Label label;
+
+    public void initialize() {
+        // TODO
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        label.setText("HOLA MUNDO!");
+    }
+}

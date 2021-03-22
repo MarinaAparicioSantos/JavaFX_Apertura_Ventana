9de7dd38ed9bef38152b77a86afde6076efad631
MarinaAparicioSantos <marina.aparicio.santos.al@iespoligonosur.org>
2021-03-22 23:19:04 +0100

    22/03

--- src/main/java/gui/viewsandcontrollers/main/MainController.java
+++ src/main/java/gui/viewsandcontrollers/main/MainController.java
@@ -1,20 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package gui.viewsandcontrollers.main;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-public class MainController {
-	  @FXML
-	    private Label label;
 
-	    public void initialize() {
-	        // TODO
-	    }
+public class MainController implements Initializable {
 
-	    @FXML
-	    private void handleButtonAction(ActionEvent event) {
-	        label.setText("Hello World!");
-	    }
+    @FXML
+    private Button btnAbrir;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void abrirVentanaHija(ActionEvent event) throws IOException {
+
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("viewsandcontrollers/model/Model.fxml"));
+
+
+            Parent root = loader.load();
+
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.showAndWait();
+
+       
+
+    }
 
 }
